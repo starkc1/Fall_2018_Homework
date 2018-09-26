95da38a07e06e9860d1574ab015e177336d81daf
Cameron Stark <starkc920@outlook.com>
2018-09-26 08:00:26 -0400

    new code

--- CS-332/Stark_P1_New.rkt
+++ CS-332/Stark_P1_New.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(define rectList '((2 10 7 7) (6 8 12 5) (1 4 6 1) (5 4 11 1)))
+(define recList '((2 10 7 7) (6 8 12 5) (1 4 6 1) (5 4 11 1)))
 (define test '(1 2 3 4))
 
 (define (main mainList)
@@ -10,7 +10,29 @@
 )
 
 (define (checkIntersection rect1 rectList)
-  '()
+  (if (equal? (length rect1) 0)
+      '()
+      (if (or (equal? (checkLeft (firstX rect1) (firstX (first rectList))) #f) (equal? (checkLeft (secondX (first rectList)) (secondX rect1)) #f))
+          '()
+          (if (or (equal? (checkAbove  (topY rect1) (bottomY (first rectList))) #f) (equal? (checkAbove (topY (first rectList)) (bottomY rect1)) #f))
+              
+          )
+      )
+  )
+)
+
+(define (checkLeft x1 x2)
+  (if (> x1 x2)
+      #f
+      #t
+  )
+)
+
+(define (checkAbove y1 y2)
+  (if (< y1 y2)
+      #f
+      #t
+  )
 )
 
 (define (firstX rect)
@@ -23,4 +45,8 @@
 
 (define (topY rect)
   (first(rest rect))
+)
+
+(define (bottomY rect)
+  (first(rest(rest(rest rect))))
 )
