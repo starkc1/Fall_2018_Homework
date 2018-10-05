9f1b9214b76e95d0b6413734b237bca5a5d5b2aa
Cameron Stark <starkc920@outlook.com>
2018-10-04 23:37:24 -0400

    add new homwork assignment

---
+++ CS-315/Homework_3/Homework3_CMS.java
@@ -0,0 +1,18 @@
+// Author: Cameron Stark
+// Class: CS 315
+// Assignment: Homework 3
+
+public class BinaryCodes {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void generateBinaryCode() {
+
+    }
+
+    public static void generateGrayCode() {
+        
+    }
+}
